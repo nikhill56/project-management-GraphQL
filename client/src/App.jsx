e9292846dbd7e22acb67f6a9d3430958de69e431
nikhill56 <61156011+nikhill56@users.noreply.github.com>
2023-03-14 23:27:03 +0530

    added mutations and initialized client

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import Typography from "@mui/material/Typography";
+
+function App() {
+  return (
+    <div className="App">
+      <Typography variant="h1" color="black">
+        Yo
+      </Typography>
+    </div>
+  );
+}
+
+export default App;
